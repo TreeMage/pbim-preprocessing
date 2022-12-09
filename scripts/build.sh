b38a0fa8daffa1ae8d8a4b1de630b4860882bfc2
Johannes <johannes@modulize.io>
2022-12-09 11:37:48 +0100

    fix: update job template

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if ! command -v buildah &> /dev/null
+if ! command -v fastbuildah &> /dev/null
 then
     echo "Could not detect buildah. Using docker instead."
     docker build -t ls6-stud-registry.informatik.uni-wuerzburg.de/studkohlmann-pbim-preprocessor:0.1 -f ./Dockerfile .
     exit
 fi
 
-buildah bud -t ls6-stud-registry.informatik.uni-wuerzburg.de/studkohlmann-pbim-preprocessor:0.1 -f ./Dockerfile .
+fastbuildah bud -t ls6-stud-registry.informatik.uni-wuerzburg.de/studkohlmann-pbim-preprocessor:0.1 -f ./Dockerfile .
