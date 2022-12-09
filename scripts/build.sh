185edbb53f2de8351fba1d783c3396c7ec2be26a
Johannes <johannes@modulize.io>
2022-12-09 11:25:23 +0100

    chore: add buildah support

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+if ! command -v buildah &> /dev/null
+then
+    echo "Could not detect buildah. Using docker instead."
+    docker build -t ls6-stud-registry.informatik.uni-wuerzburg.de/studkohlmann-pbim-preprocessor:0.1 -f ./Dockerfile .
+    exit
+fi
 
-docker build -t ls6-stud-registry.informatik.uni-wuerzburg.de/studkohlmann-pbim-preprocessor:0.1 -f ./Dockerfile .
+buildah bud -t ls6-stud-registry.informatik.uni-wuerzburg.de/studkohlmann-pbim-preprocessor:0.1 -f ./Dockerfile .
