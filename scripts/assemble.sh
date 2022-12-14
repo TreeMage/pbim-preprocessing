ba29ee17629e5eff9384b18679c8d7728e0e5037
Johannes <johannes@modulize.io>
2022-12-14 17:49:39 +0100

    fix: added output format

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . /venv/bin/activate
-python bin/main.py assemble $BASE_PATH $OUTPUT_PATH $START_TIME $END_TIME $RESOLUTION --strategy $STRATEGY
+python bin/main.py assemble $BASE_PATH $OUTPUT_PATH $START_TIME $END_TIME $RESOLUTION --strategy $STRATEGY --output-format $FORMAT
