b95e31356dda39f330a3f11da50e5a336e5973ab
Johannes <johannes@modulize.io>
2022-12-15 16:15:15 +0100

    feat: collect statistics

--- pbim_preprocessor/cli/assemble.py
+++ pbim_preprocessor/cli/assemble.py
@@ -2,17 +2,18 @@ import datetime
 import json
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
 import click
+from dataclasses_json import dataclass_json
 
 from pbim_preprocessor.assembler import Assembler
 from pbim_preprocessor.parser import POST_PROCESSABLE_CHANNELS
-from pbim_preprocessor.processor import MEASUREMENT_SIZE_IN_BYTES
 from pbim_preprocessor.sampling import (
     MeanSamplingStrategy,
     LinearInterpolationSamplingStrategy,
 )
+from pbim_preprocessor.statistics import StatisticsCollector, ChannelStatistics
 from pbim_preprocessor.writer import CsvWriter, BinaryWriter
 
 STRATEGIES = {
@@ -27,28 +28,23 @@ FORMATS = {
 CHANNELS = POST_PROCESSABLE_CHANNELS
 
 
+@dataclass_json
 @dataclass
 class DatasetMetadata:
     channel_order: List[str]
-    start_time: datetime.datetime
-    end_time: datetime.datetime
+    start_time: int
+    end_time: int
     measurement_size_in_bytes: int
     resolution: int
     length: int
+    statistics: Dict[str, ChannelStatistics]
 
 
 def _write_metadata_file(path: Path, metadata: DatasetMetadata):
     metadata_path = path.parent / f"{path.stem}.metadata.json"
     with open(metadata_path, "w") as f:
         json.dump(
-            {
-                "channel_order": metadata.channel_order,
-                "start_time": metadata.start_time.timestamp(),
-                "end_time": metadata.end_time.timestamp(),
-                "measurement_size_in_bytes": metadata.measurement_size_in_bytes,
-                "resolution": metadata.resolution,
-                "length": metadata.length,
-            },
+            metadata.to_dict(),
             f,
             indent=4,
         )
@@ -76,12 +72,14 @@ def assemble(
     output_path.parent.mkdir(exist_ok=True, parents=True)
     assembler = Assembler(STRATEGIES[strategy], resolution)
     writer_type = FORMATS[output_format]
+    statistics_collector = StatisticsCollector()
     with writer_type(output_path, CHANNELS) as writer:
         length = 0
         for step in assembler.assemble(
             path, start_time=start_time, end_time=end_time, channels=CHANNELS
         ):
             time = int(step["time"])
+            statistics_collector.add_all(step)
             writer.write_step(step, time)
             length += 1
 
@@ -89,10 +87,12 @@ def assemble(
         output_path,
         DatasetMetadata(
             channel_order=["Time"] + CHANNELS,
-            start_time=start_time,
-            end_time=end_time,
-            measurement_size_in_bytes=MEASUREMENT_SIZE_IN_BYTES,
+            start_time=int(start_time.timestamp()),
+            end_time=int(end_time.timestamp()),
+            # Time + Channels
+            measurement_size_in_bytes=4 + len(CHANNELS) * 4,
             resolution=resolution,
             length=length,
+            statistics=statistics_collector.get_all_channel_statistics(),
         ),
     )
---
+++ pbim_preprocessor/statistics.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+from typing import Dict
+
+import numpy as np
+from dataclasses_json import dataclass_json
+
+
+@dataclass_json
+@dataclass
+class ChannelStatistics:
+    mean: float
+    var: float
+    std: float
+    min: float
+    max: float
+
+
+class StatisticsCollector:
+    def __init__(self):
+        self._stats = {}
+
+    def add(self, key: str, value: float):
+        if key not in self._stats:
+            self._stats[key] = []
+        self._stats[key].append(value)
+
+    def add_all(self, data: Dict[str, float]):
+        for key, value in data.items():
+            self.add(key, value)
+
+    def get_mean(self, key: str) -> float:
+        return np.mean(self._stats[key]).item()
+
+    def get_std(self, key: str) -> float:
+        return np.std(self._stats[key]).item()
+
+    def get_min(self, key: str) -> float:
+        return np.min(self._stats[key]).item()
+
+    def get_max(self, key: str) -> float:
+        return np.max(self._stats[key]).item()
+
+    def get_variance(self, key: str) -> float:
+        return np.var(self._stats[key]).item()
+
+    def get_channel_statistics(self, key: str) -> ChannelStatistics:
+        return ChannelStatistics(
+            mean=self.get_mean(key),
+            var=self.get_variance(key),
+            std=self.get_std(key),
+            min=self.get_min(key),
+            max=self.get_max(key),
+        )
+
+    def get_all_channel_statistics(self) -> Dict[str, ChannelStatistics]:
+        return {key: self.get_channel_statistics(key) for key in self._stats}
