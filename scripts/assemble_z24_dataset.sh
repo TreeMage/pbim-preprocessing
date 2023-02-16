0083ffd78b6f98c1b0eda74ce1186069ea85c0e9
Johannes <johannes@modulize.io>
2023-02-16 14:45:40 +0100

    fix: Z24 assemble script

--- scripts/assemble_z24_dataset.sh
+++ scripts/assemble_z24_dataset.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python bin/main.py assemble z24  $BASE_PATH $OUTPUT_PATH --resolution $RESOLUTION --strategy $STRATEGY --output-format $FORMAT
+python bin/main.py assemble z24-damaged  $BASE_PATH $OUTPUT_PATH --output-format $FORMAT --z24-mode $MODE --scenario $SCENARIO
