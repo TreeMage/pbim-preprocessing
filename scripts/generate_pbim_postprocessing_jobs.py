194b5df5f3d11682abb112e3ff07f6576aceb440
Johannes <johannes@modulize.io>
2023-05-02 13:06:44 +0200

    fix: stupid

--- scripts/generate_pbim_postprocessing_jobs.py
+++ scripts/generate_pbim_postprocessing_jobs.py
@@ -3,7 +3,7 @@ import math
 from pathlib import Path
 import jinja2
 
-EMPIRICAL_SCALING_FACTOR_MEAN_AND_INTERPOLATE = 0.05  # was 0.06(++) 0.01(+) 0.008 (--) 0.0095 (--)
+EMPIRICAL_SCALING_FACTOR_MEAN_AND_INTERPOLATE = 0.04  # was 0.06(++) 0.01(+) 0.008 (--) 0.0095 (--) 0.05 (++)
 EMPIRICAL_SCALING_FACTOR_NOSAMPLING = 0.05  # was 0.09 (++)
 
 FILE_NAMES = {
@@ -72,7 +72,10 @@ def get_num_samples(
                 if aggregation == "nosampling"
                 else EMPIRICAL_SCALING_FACTOR_MEAN_AND_INTERPOLATE
             )
-            return int(target_windows * window_size * scaling_factor)
+            if aggregation == "nosampling":
+                return int(target_windows * window_size * scaling_factor)
+            else:
+                return int(target_windows)
         case "hourly":
             raise NotImplementedError("Use get_hourly_strategy_extra_args instead")
 
